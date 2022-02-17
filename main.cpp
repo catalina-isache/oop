67a26c86cef7b2d505ecda8a6870cd93afa0d4a1
catalina-isache <maria-catalina.isache@s.unibuc.ro>
2022-02-17 11:22:10 +0200

    prob2

--- main.cpp
+++ main.cpp
@@ -1,18 +1,71 @@
 #include<iostream>
+#include<cstring>
+#include<cmath>
 using namespace std;
-
+float a[100],b[100];
 int main()
 {
-    int a,b,i,d,s;
-    cin>>a>>b;
-    for(i=a; i<=b;i++)
+    float raza,h,l,abaza,alat,frisca,gem,lmic,lmare,cmica,cmare,totalgem=0.0,totalfrisca=0.0;
+    char s[10],baza[10];
+    int i=0,x;
+    while(1)
     {
-        s=1;
-        for (d=2;d<=i/2;d++)
-            if(i%d==0)
-            s+=d;
-        if(s==i)
-            cout<<i<<'\n';
+        i++;
+        cin>>s;
+        if (strcmp(s,"ADD")==0)
+        {
+            cin>>baza;
+            if(strcmp(baza,"cerc")==0)
+            {
+                cin>>raza>>h;
+                abaza=3.14*raza*raza;
+                alat=2*3.14*raza*h;
+                frisca=abaza*2+alat;
+                gem=2*abaza*h;
+
+            }
+            if(strcmp(baza,"dreptunghi")==0)
+            {
+                cin>>lmic>>lmare>>h;
+                abaza=lmic*lmare;
+                frisca=2*(abaza+h*lmic+h*lmare);
+                gem=2*abaza*h;
+            }
+            if(strcmp(baza,"patrat")==0)
+            {
+                cin>>l>>h;
+                abaza=l*l;
+                frisca=2*abaza+4*l*h;
+                gem=2*h*abaza;
+            }
+            if(strcmp(baza,"triunghi")==0)
+            {
+                cin>>cmica>>cmare>>h;
+                abaza=cmica*cmare/2;
+                frisca=2*abaza+h*cmica+h*cmare+h*sqrt(cmica*cmica+cmare*cmare);
+                gem=abaza*h*2;
+            }
+            cout<<"gem: "<<gem<<" frisca: "<<frisca<<'\n';
+            a[i]=gem;
+            b[i]=frisca;
+            totalgem+=gem;
+            totalfrisca+=frisca;
+
+        }
+        if (strcmp(s,"REMOVE")==0)
+        {
+            cin>>x;
+            totalgem-=a[x];
+            totalfrisca-=b[x];
+            a[x]=0;
+            b[x]=0;
+        }
+        if (strcmp(s,"TOTAL")==0)
+        {
+            cout<<"gem total: "<<totalgem<<" frisca totala: "<<totalfrisca<<'\n';
+        }
+        if (strcmp(s,"STOP")==0)
+            break;
     }
 
 }
