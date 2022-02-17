2ee45a4a5f685d4ece0e5275d8f0a66a149c1cdd
catalina-isache <maria-catalina.isache@s.unibuc.ro>
2022-02-17 10:41:41 +0200

    problema

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, world!\n";
-    return 0;
+int main()
+{
+    int a,b,i,d,s;
+    cin>>a>>b;
+    for(i=a; i<=b;i++)
+    {
+        s=1;
+        for (d=2;d<=i/2;d++)
+            if(i%d==0)
+            s+=d;
+        if(s==i)
+            cout<<i<<'\n';
+    }
+
 }
+
